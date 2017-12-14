5f53974ffee62e7d3a46021dc634b24ce37166cc
Chris Edmonds <chris.edmonds128@gmail.com>
2017-12-14 15:18:38 -0700

    re

--- beta/spiders/isplack_crawl.py
+++ beta/spiders/isplack_crawl.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import datetime
-#import boto3
+import boto3
 
-#s3 = boto3.resource('s3')
+s3 = boto3.resource('s3')
 
 class IsplackCrawlSpider(scrapy.Spider):
     name = 'isplack-crawl'
